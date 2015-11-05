ccef372ca89df0c0876549aa4bf9cc30aba34989
maston14 <maston14@hotmail.com>
2015-11-05 22:25:49 +0800

    fix2.0

--- Stick-Hero/LoginViewController.swift
+++ Stick-Hero/LoginViewController.swift
@@ -21,6 +21,41 @@ class LoginViewController: UIViewController {
         let un = username.text!
         let pw = password.text!
         
+        struct Response: JSONJoy {
+            let status: String?
+            let error: String?
+            init(_ decoder: JSONDecoder) {
+                status = decoder["status"].string
+                error = decoder["error"].string
+            }
+        }
+        
+        let params = ["username":"\(username.text!)", "password":"\(password.text!)"]
+        
+        
+        do {
+            let opt = try HTTP.POST("http://192.168.1.104/testreg.php", parameters: params, requestSerializer: JSONParameterSerializer())
+            opt.start { response in
+                print(response.description)
+                if let error = response.error {
+                    print("got an error: \(error)")
+                    return
+                }
+                let resp = Response(JSONDecoder(response.data))
+                if let err = resp.error {
+                    print("got an error: \(err)")
+                }
+                if let status = resp.status {
+                    print("completed: \(status)")
+                }
+            }
+        }
+        catch let error {
+            print("got an error creating the request: \(error)")
+        }
+
+        
+        
         self.performSegueWithIdentifier("login", sender: self)
         
     }
