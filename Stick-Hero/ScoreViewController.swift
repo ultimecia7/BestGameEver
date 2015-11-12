3ba23fc1fb666499ee816a96644b11d0aad3de94
maston14 <maston14@hotmail.com>
2015-11-12 15:25:12 +0800

    basic

---
+++ Stick-Hero/ScoreViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ScoreViewController.swift
+//  Stick-Hero
+//
+//  Created by YIZHONGQI on 11/12/15.
+//  Copyright © 2015 koofrank. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import AVFoundation
+
+class ScoreViewController: UIViewController {
+}
