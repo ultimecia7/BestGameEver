107b1948b127a7c1f878ac733e538532ed897a3d
ultimecia7 <ultimecia7@gmail.com>
2015-12-21 16:14:06 +0800

    Retrieve Friend List in ContactViewController

--- Stick-Hero/ContactViewController.swift
+++ Stick-Hero/ContactViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 import SpriteKit
 import AVFoundation
+import SwiftHTTP
+import JSONJoy
 
 class ContactViewController: UITableViewController{
     
+    struct ArrayResponse: JSONJoy{
+        let friendList: Array<JSONDecoder>?
+        init(_ decoder: JSONDecoder) {
+            friendList = decoder["friendList"].array
+        }
+    }
+    
+    struct ArrayElement: JSONJoy{
+        let username : String?
+        let highscore: String?
+        let speed_highscore: String?
+        let online: String?
+        let last_login_time: String?
+        init(_ decoder: JSONDecoder) {
+            username = decoder["username"].string
+            highscore = decoder["highscore"].string
+            speed_highscore = decoder["speed_highscore"].string
+            online = decoder["online"].string
+            last_login_time = decoder["last_login_time"].string
+        }
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("shit")
+        let params = ["username":"root"]
+        do {
+            let opt = try HTTP.POST("http://192.168.1.102/friend_info.php", parameters: params, requestSerializer: JSONParameterSerializer())
+            opt.start { response in
+                print(response.description)
+                if let error = response.error {
+                    print("got an error: \(error)")
+                    return
+                }
+                let resp = ArrayResponse(JSONDecoder(response.data))
+                for var friend in resp.friendList!{
+                    let str = ArrayElement(friend)
+                    
+                }
+                            }
+        }
+        catch let error {
+            print("got an error creating the request: \(error)")
+        }
+
     }
     
     override func viewWillAppear(animated: Bool) {
