e88b63f890cfdcba48a7de61f029b9b78f0db0dd
ultimecia7 <ultimecia7@gmail.com>
2015-12-21 19:10:58 +0800

    Try the Wx API to share results on we chat

---
+++ Stick-Hero/bridge.h
@@ -0,0 +1,11 @@
+//
+//  bridge.h
+//  Stick-Hero
+//
+//  Created by 徐静恺 on 12/21/15.
+//  Copyright © 2015 koofrank. All rights reserved.
+//
+
+#import "WechatAuthSDK.h"
+#import "WXApi.h"
+#import "WXApiObject.h"
