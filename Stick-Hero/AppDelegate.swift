bb8b1d0ff43bed3b1605a9f4544f2a5f69bf7923
maston14 <maston14@hotmail.com>
2015-12-21 23:33:54 +0800

    conflict

--- Stick-Hero/AppDelegate.swift
+++ Stick-Hero/AppDelegate.swift
@@ -9,37 +9,42 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate{
+class AppDelegate: UIResponder, UIApplicationDelegate, WXApiDelegate{
 
     var window: UIWindow?
-
-
-
-
+    
+    func application(application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+            // 注册app(这里随便取个名字)
+            WXApi.registerApp("hangge_appid")
+            return true
+    }
+    
     func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
     //微信分享完毕后的回调（只有使用真实的AppID才能收到响应）
-    
+    func onResp(resp: BaseResp!) {
+        if resp.isKindOfClass(SendMessageToWXResp){//确保是对我们分享操作的回调
+            if resp.errCode == WXSuccess.rawValue{//分享成功
+                NSLog("分享成功")
+            }else{//分享失败
+                NSLog("分享失败，错误码：%d, 错误描述：%@", resp.errCode, resp.errStr)
+            }
+        }
+    }
 
 }
 
